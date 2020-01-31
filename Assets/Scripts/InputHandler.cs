1845c2a63fadf6eb30ed3c8f916c87b8f7fed2b1
Daniel Oosterwijk <danieloosterwijk@gmail.com>
2020-01-31 21:03:42 +1300

    Add frictionless player in a vacuum.

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class InputHandler
+{
+    public static Vector3 MovementDirection
+    {
+        get
+        {
+            Vector3 result = new Vector3(
+                Input.GetAxis("Horizontal"),
+                0,
+                Input.GetAxis("Vertical")
+            );
+
+            if (result.sqrMagnitude > 0.05)
+            {
+                return result.normalized;
+            }
+            else
+            {
+                return Vector3.zero;
+            }
+        }
+    }
+
+    public static bool IsInteracting
+    {
+        get
+        {
+            return Input.GetAxis("Interact") > 0.05;
+        }
+    }
+}
