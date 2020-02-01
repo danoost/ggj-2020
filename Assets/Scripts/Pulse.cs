89aaa407c69002d5de4afde1031ac855e1402735
Jacob Cheatley (AWARE) <jacob.cheatley@awaregroup.com>
2020-02-02 11:46:33 +1300

    Sexy title screen thanks Kelsey

---
+++ Assets/Scripts/Pulse.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pulse : MonoBehaviour
+{
+    [SerializeField] private float minScaleFactor = 0.9f;
+    [SerializeField] private float sf = 1;
+    private float maxScaleFactor;
+    private Vector3 baseScale;
+    private float currentScaleFactor = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        baseScale = transform.localScale;
+        maxScaleFactor = 1 / minScaleFactor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentScaleFactor = Mathf.Lerp(minScaleFactor, maxScaleFactor, (Mathf.Sin(sf * Time.realtimeSinceStartup) + 1) / 2);
+        transform.localScale = currentScaleFactor * baseScale;
+    }
+}
---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    [SerializeField] private float sf = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, sf * Time.deltaTime);
+    }
+}
