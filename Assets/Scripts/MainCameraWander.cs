9919168671c42912aed7cbd5b6792e4576991aea
Jacob Cheatley (AWARE) <jacob.cheatley@awaregroup.com>
2020-02-02 11:56:50 +1300

    SPIN

---
+++ Assets/Scripts/MainCameraWander.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraWander : MonoBehaviour
+{
+    [SerializeField] float radius = 30;
+    [SerializeField] float speed = 2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Mathf.Cos(speed * Time.realtimeSinceStartup) * radius;
+        float y = Mathf.Sin(speed * Time.realtimeSinceStartup) * radius;
+        transform.position = new Vector3(x, y, transform.position.z);
+    }
+}
