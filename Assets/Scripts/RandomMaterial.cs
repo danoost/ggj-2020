2ec1509012616c59b5956e0eb7759268bdf0f914
Daniel Oosterwijk <danieloosterwijk@gmail.com>
2020-02-02 11:03:39 +1300

    Added booster materials, random selection and rot.

---
+++ Assets/Scripts/RandomMaterial.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class RandomMaterial : MonoBehaviour
+{
+    [SerializeField]
+    private Material[] mats;
+
+    private MeshRenderer meshRenderer;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+
+        meshRenderer.material = mats[Random.Range(0, mats.Length)];
+    }
+}
