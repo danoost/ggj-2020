0bdc9e74254fa84697f336bbf0f10ca1778a5866
Jacob Cheatley (AWARE) <jacob.cheatley@awaregroup.com>
2020-02-02 09:39:42 +1300

    Added start splash screen

--- Assets/Scripts/PlayerJoin.cs
+++ Assets/Scripts/PlayerJoin.cs
@@ -3,14 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
 using System.Linq;
+using TMPro;
 
 public class PlayerJoin : MonoBehaviour
 {
     [SerializeField] private InputActionAsset asset;
     [SerializeField] private Camera previewCamera;
+    [SerializeField] private Canvas canvas;
+    [SerializeField] private TextMeshProUGUI joinedText;
+    [SerializeField] private TextMeshProUGUI startPrompt;
 
     public Material[] playerMats;
     public Color[] playerColors;
+    public string[] numbers;
 
     private InputAction interactAction;
     private List<NewPlayerInfo> playersJoined;
@@ -20,21 +25,37 @@ public class PlayerJoin : MonoBehaviour
     {
         instance = this;
         playersJoined = new List<NewPlayerInfo>();
+        numbers = new string[] { "One", "Two", "Three", "Four" };
+
         interactAction = asset.FindAction("InteractPressed");
         interactAction.performed += JoinPlayer;
         interactAction.Enable();
     }
 
+    protected void Start()
+    {
+        startPrompt.gameObject.SetActive(false);
+    }
+
     private void JoinPlayer(InputAction.CallbackContext context)
     {
-        if (GameFlowManager.instance.state != GameState.WAITING_FOR_PLAYERS)
+        if (GameFlowManager.instance.state != GameState.WAITING_FOR_PLAYERS || playersJoined.Count >= 4)
             return;
 
         InputDevice device = context.control.device;
         if (playersJoined.TrueForAll(pi => pi.device != device))
         {
             // New device! Create a player
-            playersJoined.Add(new NewPlayerInfo { device = device, color = DynamicMaterialColor.CreateRandomHue() });
+            playersJoined.Add(new NewPlayerInfo { device = device });
+            int index = playersJoined.Count - 1;
+            string hex = ColorUtility.ToHtmlStringRGB(playerColors[index]);
+            string space = index == 0 ? "" : "<space=2em>";
+            joinedText.SetText(joinedText.text + $"{space}<color=#{hex}>Player {numbers[index]}</color>");
+
+            if (playersJoined.Count > 1)
+            {
+                startPrompt.gameObject.SetActive(true);
+            }
         } else
         {
             StartGame();
@@ -44,6 +65,7 @@ public class PlayerJoin : MonoBehaviour
     private void StartGame()
     {
         GameFlowManager.instance.state = GameState.PLAYING;
+        canvas.gameObject.SetActive(false);
         previewCamera.transform.rotation = Quaternion.Euler(180, 0, 0);
         previewCamera.clearFlags = CameraClearFlags.SolidColor;
         int playerIndex = 0;
---
+++ Assets/Scripts/TextRainbow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TextRainbow : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI text;
+    private float currentHue;
+
+    private void Start()
+    {
+        currentHue = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Color color = Color.HSVToRGB(currentHue, 1, 1);
+        currentHue = (currentHue + Time.deltaTime) % 1f;
+        text.color = color;
+    }
+}
