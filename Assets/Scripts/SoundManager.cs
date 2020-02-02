2a03b5a2b2aca01ca549fb07903f4b1d9f2a7a89
Jacob Cheatley (AWARE) <jacob.cheatley@awaregroup.com>
2020-02-02 13:21:29 +1300

    Add sound manager

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] shootClips;
+    [SerializeField] private AudioClip[] explodeClips;
+    [SerializeField] private AudioClip[] attachClips;
+    [SerializeField] private AudioClip[] joinClips;
+    [SerializeField] private AudioClip[] winClips;
+    [SerializeField] private AudioClip[] deathClips;
+    [SerializeField] private AudioClip[] startClips;
+
+    private AudioSource source;
+    private static SoundManager instance;
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        instance = this;
+    }
+
+    public static void PlayShootClip()
+    {
+        PlayRandomClip(instance.shootClips);
+    }
+    public static void PlayExplodeClip()
+    {
+        PlayRandomClip(instance.explodeClips);
+    }
+    public static void PlayAttachClip()
+    {
+        PlayRandomClip(instance.attachClips);
+    }
+    public static void PlayJoinClip()
+    {
+        PlayRandomClip(instance.joinClips);
+    }
+    public static void PlayWinClip()
+    {
+        PlayRandomClip(instance.winClips);
+    }
+    public static void PlayDeathClip()
+    {
+        PlayRandomClip(instance.deathClips);
+    }
+    public static void PlayStartClip()
+    {
+        PlayRandomClip(instance.startClips);
+    }
+
+    private static void PlayRandomClip(AudioClip[] clips)
+    {
+        int index = Random.Range(0, clips.Length - 1);
+        instance.source.PlayOneShot(clips[index]);
+    }
+}
